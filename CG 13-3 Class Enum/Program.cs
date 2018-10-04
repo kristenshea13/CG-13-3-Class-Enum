1e77c67e429683b68db0d342bb9cb1e0ecbbdc65
Kristen Shea <kstar61@live.com>
2018-10-04 00:32:17 -0500

    *

--- CG 13-3 Class Enum/Program.cs
+++ CG 13-3 Class Enum/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CG_13_3_Class_Enum
 {
@@ -6,7 +7,167 @@ namespace CG_13_3_Class_Enum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /*Complete a Class Enum program with an enumeration of 
+             * all students in class’ first name.Ask the user to enter 
+             * their name. Using a Switch statement, print their last name.*/
+
+            List<Student> students = new List<Student>
+            {
+                new Student() {FirstName = "Kristen", LastName = LastName.Shea},
+                new Student() {FirstName = "Adriana", LastName = LastName.Disano},
+                new Student() {FirstName = "Angelique", LastName = LastName.Jacobs},
+                new Student() {FirstName = "Carla", LastName = LastName.Huggans},
+                new Student() {FirstName = "Jane", LastName = LastName.Brown},
+                new Student() {FirstName = "Michelle", LastName = LastName.Crutcher},
+                new Student() {FirstName = "Sabrina", LastName = LastName.Lindsey},
+                new Student() {FirstName = "Temeka", LastName = LastName.Spears},
+                new Student() {FirstName = "Agatha", LastName = LastName.Berwick},
+                new Student() {FirstName = "Bennita", LastName = LastName.Higgins},
+                new Student() {FirstName = "Carolina", LastName = LastName.Oppy},
+                new Student() {FirstName = "Jennifer", LastName = LastName.Faron},
+                new Student() {FirstName = "Madelyn", LastName = LastName.Weller},
+                new Student() {FirstName = "Natalia", LastName = LastName.Solano},
+                new Student() {FirstName = "Sandra", LastName = LastName.Nelson},
+                new Student() {FirstName = "Alicia", LastName = LastName.Reynolds},
+                new Student() {FirstName = "Blake", LastName = LastName.Pickering},
+                new Student() {FirstName = "Diana", LastName = LastName.Peters},
+                new Student() {FirstName = "Kendall", LastName = LastName.Union},
+                new Student() {FirstName = "Megan", LastName = LastName.Hainline},
+                new Student() {FirstName = "Natalie", LastName = LastName.Erdelt},
+                new Student() {FirstName = "Sarah", LastName = LastName.Park},
+                new Student() {FirstName = "Allison", LastName = LastName.Hix},
+                new Student() {FirstName = "Breona", LastName = LastName.Caldwell},
+                new Student() {FirstName = "Erin", LastName = LastName.Hogan},
+                new Student() {FirstName = "Kendra", LastName = LastName.Kuhl},
+                new Student() {FirstName = "Melissa", LastName = LastName.Dodd},
+                new Student() {FirstName = "Nichole", LastName = LastName.Church},
+                new Student() {FirstName = "Shelia", LastName = LastName.Wambui}
+            };
+
+            Console.Write("What is the student's first name? ");
+            var firstNameEntered = Console.ReadLine();
+
+
+
+
+            //    private static void PrintAssessment(List<Todo> todos)
+            //{
+            //    foreach (var todo in todos)
+            //    {
+            //        switch (todo.Status)
+            //        {
+            //            case Status.NotStarted:
+            //                Console.ForegroundColor = ConsoleColor.DarkRed;
+            //                break;
+            //            case Status.InProgress:
+            //                Console.ForegroundColor = ConsoleColor.DarkBlue;
+            //                break;
+            //            case Status.OnHold:
+            //                Console.ForegroundColor = ConsoleColor.Yellow;
+            //                break;
+            //            case Status.Completed:
+            //                Console.ForegroundColor = ConsoleColor.Magenta;
+            //                break;
+            //            case Status.Deleted:
+            //                Console.ForegroundColor = ConsoleColor.White;
+            //                break;
+            //            default:
+            //                break;
+            //        }
+
+
+
+
+                }
+
+        private static void PrintLastName(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                switch (Student.FirstName)
+                {
+                    case FirstName.Shelia
+                        LastName.Wambui;
+                        break;
+
+
+                }
+
+
+            }
+
+
         }
+
+            
+
+
+        }
+
+    enum LastName
+    {
+        Shea,
+        Disano,
+        Kuhl,
+        Jacobs,
+        Huggans,
+        Wambui,
+        Brown,
+        Church,
+        Crutcher,
+        Lindsey,
+        Spears,
+        Berwick,
+        Higgins,
+        Oppy,
+        Dodd,
+        Faron,
+        Weller,
+        Solano,
+        Hogan,
+        Nelson,
+        Reynolds,
+        Pickering,
+        Peters,
+        Union,
+        Hainline,
+        Erdelt,
+        Park,
+        Hix,
+        Caldwell
     }
+
+    //enum FirstName
+    //{ 
+        //Kendall,
+        //Diana,
+        //Alicia,
+        //Natalia,
+        //Blake,
+        //Shelia,
+        //Nichole,
+        //Melissa,
+        //Kendra,
+        //Erin,
+        //Breona,
+        //Allison,
+        //Sarah,
+        //Natalie,
+        //Megan,
+        //Kristen,
+        //Adriana,
+        //Madelyn,
+        //Jennifer,
+        //Bennita,
+        //Sandra,
+        //Temeka,
+        //Agatha,
+        //Carolina,
+        //Jane,
+        //Carla,
+        //Angelique,
+        //Sabrina,
+        //Michelle
+    //}
 }
---
+++ CG 13-3 Class Enum/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_13_3_Class_Enum
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public LastName LastName { get; set; }
+    }
+}
