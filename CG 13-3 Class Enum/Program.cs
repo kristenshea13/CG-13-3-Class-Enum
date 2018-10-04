464622b58b8c28974a8454d493a06d238eced1af
Kristen Shea <kstar61@live.com>
2018-10-03 23:08:24 -0500

    Add project files.

---
+++ CG 13-3 Class Enum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_13_3_Class_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
