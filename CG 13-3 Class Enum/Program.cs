323ee39b21a370a629e7344f4f35d4dc9de550bb
Kristen Shea <kstar61@live.com>
2018-10-04 20:15:23 -0500

    *

--- CG 13-3 Class Enum/Program.cs
+++ CG 13-3 Class Enum/Program.cs
@@ -12,95 +12,123 @@ namespace CG_13_3_Class_Enum
              * all students in class’ first name.Ask the user to enter 
              * their name. Using a Switch statement, print their last name.*/
 
-            var students = new List<Student>
-            {
-                new Student() {FirstName = Firstname.Kristen, LastName = Lastname.Shea},
-                new Student() {FirstName = Firstname.Adriana, LastName = Lastname.Disano},
-                new Student() {FirstName = Firstname.Angelique, LastName = Lastname.Jacobs},
-                new Student() {FirstName = Firstname.Carla, LastName = Lastname.Huggans},
-                new Student() {FirstName = Firstname.Jane, LastName = Lastname.Brown},
-                new Student() {FirstName = Firstname.Michelle, LastName = Lastname.Crutcher},
-                new Student() {FirstName = Firstname.Sabrina, LastName = Lastname.Lindsey},
-                //new Student() {FirstName = Firstname.Temeka, LastName = Lastname.Spears},
-                //new Student() {FirstName = Firstname.Agatha, LastName = Lastname.Berwick},
-                //new Student() {FirstName = Firstname.Bennita, LastName = Lastname.Higgins},
-                //new Student() {FirstName = Firstname.Carolina, LastName = Lastname.Oppy},
-                //new Student() {FirstName = Firstname.Jennifer, LastName = Lastname.Faron},
-                //new Student() {FirstName = Firstname.Madelyn, LastName = Lastname.Weller},
-                //new Student() {FirstName = Firstname.Natalia, LastName = Lastname.Solano},
-                //new Student() {FirstName = Firstname.Sandra, LastName = Lastname.Nelson},
-                //new Student() {FirstName = Firstname.Alicia, LastName = Lastname.Reynolds},
-                //new Student() {FirstName = Firstname.Blake, LastName = Lastname.Pickering},
-                //new Student() {FirstName = Firstname.Diana, LastName = Lastname.Peters},
-                //new Student() {FirstName = Firstname.Kendall, LastName = Lastname.Union},
-                //new Student() {FirstName = Firstname.Megan, LastName = Lastname.Hainline},
-                //new Student() {FirstName = Firstname.Natalie, LastName = Lastname.Erdelt},
-                //new Student() {FirstName = Firstname.Sarah, LastName = Lastname.Park},
-                //new Student() {FirstName = Firstname.Allison, LastName = Lastname.Hix},
-                //new Student() {FirstName = Firstname.Breona, LastName = Lastname.Caldwell},
-                //new Student() {FirstName = Firstname.Erin, LastName = Lastname.Hogan},
-                //new Student() {FirstName = Firstname.Kendra, LastName = Lastname.Kuhl},
-                //new Student() {FirstName = Firstname.Melissa, LastName = Lastname.Dodd},
-                //new Student() {FirstName = Firstname.Nichole, LastName = Lastname.Church},
-                //new Student() {FirstName = Firstname.Shelia, LastName = Lastname.Wambui}
-            };
+            //var students = new List<Student>
+            //{
+            //    new Student() {FirstName = Firstname.Kristen, LastName = "Shea"},
+            //    new Student() {FirstName = Firstname.Adriana, LastName = "Disano"},
+            //    new Student() {FirstName = Firstname.Angelique, LastName = "Jacobs"},
+            //new Student() {FirstName = Firstname.Carla, LastName = Lastname.Huggans},
+            //new Student() {FirstName = Firstname.Jane, LastName = Lastname.Brown},
+            //new Student() {FirstName = Firstname.Michelle, LastName = Lastname.Crutcher},
+            //new Student() {FirstName = Firstname.Sabrina, LastName = Lastname.Lindsey},
+            //new Student() {FirstName = Firstname.Temeka, LastName = Lastname.Spears},
+            //new Student() {FirstName = Firstname.Agatha, LastName = Lastname.Berwick},
+            //new Student() {FirstName = Firstname.Bennita, LastName = Lastname.Higgins},
+            //new Student() {FirstName = Firstname.Carolina, LastName = Lastname.Oppy},
+            //new Student() {FirstName = Firstname.Jennifer, LastName = Lastname.Faron},
+            //new Student() {FirstName = Firstname.Madelyn, LastName = Lastname.Weller},
+            //new Student() {FirstName = Firstname.Natalia, LastName = Lastname.Solano},
+            //new Student() {FirstName = Firstname.Sandra, LastName = Lastname.Nelson},
+            //new Student() {FirstName = Firstname.Alicia, LastName = Lastname.Reynolds},
+            //new Student() {FirstName = Firstname.Blake, LastName = Lastname.Pickering},
+            //new Student() {FirstName = Firstname.Diana, LastName = Lastname.Peters},
+            //new Student() {FirstName = Firstname.Kendall, LastName = Lastname.Union},
+            //new Student() {FirstName = Firstname.Megan, LastName = Lastname.Hainline},
+            //new Student() {FirstName = Firstname.Natalie, LastName = Lastname.Erdelt},
+            //new Student() {FirstName = Firstname.Sarah, LastName = Lastname.Park},
+            //new Student() {FirstName = Firstname.Allison, LastName = Lastname.Hix},
+            //new Student() {FirstName = Firstname.Breona, LastName = Lastname.Caldwell},
+            //new Student() {FirstName = Firstname.Erin, LastName = Lastname.Hogan},
+            //new Student() {FirstName = Firstname.Kendra, LastName = Lastname.Kuhl},
+            //new Student() {FirstName = Firstname.Melissa, LastName = Lastname.Dodd},
+            //new Student() {FirstName = Firstname.Nichole, LastName = Lastname.Church},
+            //new Student() {FirstName = Firstname.Shelia, LastName = Lastname.Wambui}
+            //};
 
             Console.Write("What is the student's first name? ");
-            var firstNameEntered = Console.ReadLine();
+            string firstNameEntered = Console.ReadLine();
+            Firstname firstnameUser;
+            firstnameUser = (Firstname)Enum.Parse(typeof(Firstname), firstNameEntered, true);
+
+            PrintLastName(firstnameUser);
 
 
+            Console.ReadLine();
+        }
 
-
-
-        
-
-    }
-
-        private static void PrintLastName(List<Student> students)
+        private static void PrintLastName(Firstname firstname)
         {
-            foreach (var student in students)
+
+
+            string lastnamePrint;
+
+            switch (firstname)
             {
-                var lastnamePrint = student.LastName;
 
-                switch (student.FirstName)
-                {
-                    
 
-                    case Firstname.Kristen:
-                       lastnamePrint = Lastname.Shea;
-                        break;
-
-                    case Firstname.Adriana:
-                        lastnamePrint = Lastname.Shea;
-                        break;
-
-                    case Firstname.Angelique:
-                        lastnamePrint = Lastname.Jacobs;
-                        break;
-
-                    default:
+                case Firstname.Kristen:
+                    lastnamePrint = "Shea";
                     break;
-                    
-                }
 
+                case Firstname.Adriana:
+                    lastnamePrint = "Disano";
+                    break;
 
+                //case Firstname.Angelique:
+                //    lastnamePrint = ;
+                //    break;
 
-
-
-
-
-                Console.WriteLine();
+                default:
+                    lastnamePrint = "";
+                    break;
 
             }
 
-            
+
+
+            Console.WriteLine(lastnamePrint);
+
+
+
+
 
         }
 
 
     }
-
-    enum Lastname
+    public enum Firstname
+    {
+        Kendall,
+        Diana,
+        Alicia,
+        Natalia,
+        Blake,
+        Shelia,
+        Nichole,
+        Melissa,
+        Kendra,
+        Erin,
+        Breona,
+        Allison,
+        Sarah,
+        Natalie,
+        Megan,
+        Kristen,
+        Adriana,
+        Madelyn,
+        Jennifer,
+        Bennita,
+        Sandra,
+        Temeka,
+        Agatha,
+        Carolina,
+        Jane,
+        Carla,
+        Angelique,
+        Sabrina,
+        Michelle
+    }
+    /*enum Lastname
         {
             Shea,
             Disano,
@@ -131,38 +159,7 @@ namespace CG_13_3_Class_Enum
             Park,
             Hix,
             Caldwell
-        }
+        }*/
 
-        enum Firstname
-        {
-            Kendall,
-            Diana,
-            Alicia,
-            Natalia,
-            Blake,
-            Shelia,
-            Nichole,
-            Melissa,
-            Kendra,
-            Erin,
-            Breona,
-            Allison,
-            Sarah,
-            Natalie,
-            Megan,
-            Kristen,
-            Adriana,
-            Madelyn,
-            Jennifer,
-            Bennita,
-            Sandra,
-            Temeka,
-            Agatha,
-            Carolina,
-            Jane,
-            Carla,
-            Angelique,
-            Sabrina,
-            Michelle
-        }
+
 }
